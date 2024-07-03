9d3d6310ab692b957ba13f8e5c2c196b67639a54
Sercan Arğa <mail@sercanarga.com>
2024-07-04 01:12:10 +0300

    added /health route

---
+++ internal/routes/health.go
@@ -0,0 +1,9 @@
+package routes
+
+import "net/http"
+
+func Health(mux *http.ServeMux) {
+	mux.HandleFunc("GET /health", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	})
+}
